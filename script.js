98fdeca1fb52ed0f032d0eb2df5667cea457dd48
DanieleSavoia <danielesavoia44@gmail.com>
2023-04-14 18:16:34 +0200

    commit2

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const eleGrid = document.querySelector('.grid');
-let num = parseInt(prompt('numero quadrati'))
+const eleDiv15 = document.querySelector('.div15');
+const eleDiv3 = document.querySelector('.div3');
+const eleDiv5 = document.querySelector('.div5');
 
-for (let i = 0; i <= num; i++) {
-	console.log(i);
-	eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
+
+
+const num = 100;
+
+for(let i = 1; i <= num; i++) 
+{
+    if (i % 15 == 0){
+        eleGrid.innerHTML = eleGrid.innerHTML + `div class="cell div15">FizzBuzz</div>`;
+        console.log("FizzBuzz");
+    }else if (i % 3 == 0){
+        eleGrid.innerHTML = eleGrid.innerHTML + `div class="cell div3">Fizz</div>`;
+        console.log("Fizz");
+    }else if (i % 5 == 0){
+        eleGrid.innerHTML = eleGrid.innerHTML + `div class="cell div5">Buzz</div>`;
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
+    }
+    
 }
+
